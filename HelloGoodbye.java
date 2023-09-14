622cf3334ef297010d9c57d9b2aff4deb04cd7e2
Eesha <73623905+eesha123456789@users.noreply.github.com>
2023-09-14 10:07:36 -0700

    Add files via upload

---
+++ HelloGoodbye.java
@@ -0,0 +1,8 @@
+public class HelloGoodbye{
+    public static void main(String[] args){
+        String one= args[0];
+        String two =args[1];
+        System.out.println ("Hello"+one+"and" +two +".");
+        System.out.println ("Goodbye"+two+"and" +one +".");
+    }
+}
