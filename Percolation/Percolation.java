2df52a589786d70cc380515fe2b5fb509e9896f9
Eesha <73623905+eesha123456789@users.noreply.github.com>
2023-09-26 01:02:36 +0000

    .

--- Percolation/Percolation.java
+++ Percolation/Percolation.java
@@ -8,18 +8,51 @@ public class Percolation {
     private int top;
     private int bottom;
     private boolean[][] grid;
+    private int virtualTop;
+    private int virtualBottom;
+    private WeightedQuickUnionUF ufgrid;
+    private WeightedQuickUnionUF uffull;
+
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n){
+        verify(n,n);
         number=n;
         os=0;
         top=n*n +1;
         bottom=n*n;
         grid=new boolean[n][n];
+        virtualBottom= number+3;
+        virtualTop=number+2;
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col){
         verify(row,col);
+        grid[row][col]=true;
+        os++;
+        
+        if (isOnGrid(row - 1, col) && isOpen(row - 1, col)) {
+            ufgrid.union(flatIndex, oneDPos(row - 1, col) - 1);
+            uffull.union(flatIndex, oneDPos(row - 1, col) - 1);
+        }
+
+        if (isOnGrid(row, col - 1) && isOpen(row, col - 1)) {
+            ufgrid.union(flatIndex, oneDPos(row, col - 1) - 1);
+            uffull.union(flatIndex, oneDPos(row, col - 1) - 1);
+        }
+        
+        if (isOnGrid(row + 1, col) && isOpen(row + 1, col)) {
+            ufgrid.union(flatIndex, oneDPos(row + 1, col) - 1);
+            uffull.union(flatIndex, oneDPos(row + 1, col) - 1);
+        }
+
+        if (isOnGrid(row, col + 1) && isOpen(row, col + 1)) {
+            ufgrid.union(flatIndex, oneDPos(row, col + 1) - 1);
+            uffull.union(flatIndex, oneDPos(row, col + 1) - 1);
+        }
+
+
+
     }
 
     // is the site (row, col) open?
@@ -31,27 +64,35 @@ public class Percolation {
         else{
             return false;
         }
+        
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col){
         verify(row, col);
-
+        return ufgrid.connected(virtualTop,oneDPos(row,col));
     }
     // returns the number of open sites
     public int numberOfOpenSites(){
-
+        return os;
     }
 
     // does the system percolate?
-    public boolean percolates()
+    public boolean percolates(){
+        return ufgrid.connected(virtualTop,virtualBottom);
+    }
 
     public boolean verify(int row, int column){
         if(number>column||number>row||0>=column||0>=row){
             throw new IllegalArgumentException("Out of bounds");  
         }
     }
+    public int oneDPos(int row, int column){
+        return number*(row-1)+column;
+    }
 
-    // test client (optional)
-    public static void main(String[] args)
+    /* test client (optional)
+    public static void main(String[] args){
+
+    }*/
 }
--- Percolation/PercolationStats.java
+++ Percolation/PercolationStats.java
@@ -10,13 +10,13 @@ public class PercolationStats {
     public PercolationStats(int n, int trials){
         trial=trials;
         number=n;
-        what=new double[trials]
+        what=new double[trials];
         if (number > 0 && trial > 0) {
             for (int i=0;i<trial;i++){
                 Percolation fin=new Percolation(number);
                 while(!percolation.percolates()){
                     int x = StdRandom.uiform(1, number+1);
-                    percolation.open(x,x)
+                    percolation.open(x,x);
                 }
                 int os =percolation.numberOfOpenSites();
                 what[trial]=(double)os/(Math.pow(2,number));
@@ -40,14 +40,14 @@ public class PercolationStats {
 
     // low  endpoint of 95% confidence interval
     public double confidenceLo() {
-        int a=1.96 * stddev()
+        int a=1.96 * stddev();
         return mean() - (a/ Math.sqrt(trial));
 
     }
 
     // high endpoint of 95% confidence interval
     public double confidenceHi() {
-        int a=1.96 * stddev()
+        int a=1.96 * stddev();
         return mean() + (a / Math.sqrt(trial));
     }
 
@@ -58,3 +58,4 @@ public class PercolationStats {
     String.out.println("stddev                  = " + stddev());
     String.out.println("95% confidence interval = " + confidenceLo()+", "+confidenceHi());
    }
+}
