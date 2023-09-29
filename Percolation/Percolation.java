785309af6253238631e9437d43b7b228a185b7b1
Eesha <73623905+eesha123456789@users.noreply.github.com>
2023-09-29 03:05:02 +0000

    .

--- Percolation/Percolation.java
+++ Percolation/Percolation.java
@@ -11,12 +11,12 @@ public class Percolation {
     private int numOpen;
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
-        if(n<=0){
+        if (n <= 0) {
             throw new IllegalArgumentException("Out of bounds");
         }
         dimension=n;
-        headIndex=0;
-        tailIndex=n+1;
+        headIndex=-1;
+        tailIndex=n;
         openStatus=new boolean[n][n];
         numOpen=0;
         wquuf=new WeightedQuickUnionUF(n*n +1);
@@ -31,31 +31,31 @@ public class Percolation {
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
         verify(row, col);
-        openStatus[row][col]=true;
+        openStatus[row-1][col-1]=true;
         numOpen++;
-        if(row==1){
-            wquuf.union(headIndex,getIndex(row, col));
-            normalQU.union(headIndex,getIndex(row, col));
+        if(row==0){
+            wquuf.union(headIndex,getIndex(row-1, col-1));
+            normalQU.union(headIndex,getIndex(row-1, col-1));
         }
-        if(row==dimension){
-            wquuf.union(getIndex(row,col),tailIndex);
-            normalQU.union(getIndex(row,col),tailIndex);
+        if(row==dimension-1){
+            wquuf.union(getIndex(row-1,col-1),tailIndex);
+            normalQU.union(getIndex(row-1,col-1),tailIndex);
         }
-        if(isOpen(row-1,col)){
-            wquuf.union(getIndex(row-1,col),getIndex(row,col));
-            normalQU.union(getIndex(row-1,col),getIndex(row,col));
+        if(isOpen(row-2,col-1)){
+            wquuf.union(getIndex(row-2,col-1),getIndex(row-1,col-1));
+            normalQU.union(getIndex(row-2,col-1),getIndex(row-1,col-1));
+        }
+        if(isOpen(row-1,col-2)){
+            wquuf.union(getIndex(row-1,col-2),getIndex(row-1,col-1));
+            normalQU.union(getIndex(row-1,col-2),getIndex(row-1,col-1));
         }
         if(isOpen(row,col-1)){
-            wquuf.union(getIndex(row-1,col),getIndex(row,col));
-            normalQU.union(getIndex(row-1,col),getIndex(row,col));
+            wquuf.union(getIndex(row,col-1),getIndex(row-1,col-1));
+            normalQU.union(getIndex(row,col-1),getIndex(row-1,col-1));
         }
-        if(isOpen(row+1,col)){
-            wquuf.union(getIndex(row-1,col),getIndex(row,col));
-            normalQU.union(getIndex(row-1,col),getIndex(row,col));
-        }
-        if(isOpen(row,col+1)){
-            wquuf.union(getIndex(row-1,col),getIndex(row,col));
-            normalQU.union(getIndex(row-1,col),getIndex(row,col));
+        if(isOpen(row-1,col)){
+            wquuf.union(getIndex(row-1,col),getIndex(row-1,col-1));
+            normalQU.union(getIndex(row-1,col),getIndex(row-1,col-1));
         }
         //See the slide in the Union-Find Applications about how to do open - slide 59/60 about 7:10 in.
         //Check the top and bottom
@@ -67,14 +67,14 @@ public class Percolation {
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
         verify(row, col);
-        return openStatus[row][col];
+        return openStatus[row-1][col-1];
         // Your code should maintain this state in a data structure of boolean values
         // One liner returns the state
     }
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
         verify(row,col);
-        return wquuf.find(getIndex(row,col))==wquuf.find(headIndex);
+        return wquuf.find(getIndex(row-1,col-1))==wquuf.find(headIndex);
 
         // Is there a path from the top to the specified Index?
     }
@@ -85,10 +85,10 @@ public class Percolation {
     // does the system percolate?
     public boolean percolates() {
        //Is there a path between top and bottom? 
-        return wquuf.find(tailIndex)=wquuf.find(headIndex);
+        return wquuf.find(tailIndex)==wquuf.find(headIndex);
     }
-    public void verify(int row, int col){
-        if(row>dimension || row<=0 || col>dimension || col<=0){
+    private void verify(int row, int col){
+        if(row>=dimension || row<0 || col>=dimension || col<0){
             throw new IllegalArgumentException("Out of Bounds");
         }
     }
