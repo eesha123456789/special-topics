085d56025497b854dd26110a819790e076f43849
Eesha <73623905+eesha123456789@users.noreply.github.com>
2023-10-10 04:54:17 +0000

    .

---
+++ Percolation/Deque1.java
@@ -0,0 +1,117 @@
+public class Deque<Item> implements Iterable<Item> {
+    public Item[] list;
+    // construct an empty deque
+    public Deque(){
+        list = new Item[1];
+
+    }
+
+    // is the deque empty?
+    public boolean isEmpty(){
+        return list[0] == null;
+    }
+
+    // return the number of items on the deque
+    public int size(){
+        int a=0;
+        for(int i = 0; i<size();i++){
+            if(list[i]!=null){
+                a++;
+            }
+        }
+        return a;
+    }
+
+    // add the item to the front
+    public void addFirst(Item item){
+        if(item==null){
+            throw new IllegalArgumentException("item is null");
+        }
+        Item[] copy=new Item[size()];
+        copy=list;
+        if(size()+1>=(size()*2)){
+            list=new Item[size()*2];
+            list[0]=item;
+            for(int i=0;i<size()/2-1;i++){
+                list[i+1]=copy[i];
+            }
+        }
+        else{
+            list[0]=item;
+            for(int i=0;i<size()/2-1;i++){
+                list[i+1]=copy[i];
+            }
+        }
+    }
+
+    // add the item to the back
+    public void addLast(Item item){
+        if(item==null){
+            throw new IllegalArgumentException("item is null");
+        }
+        Item[] copy=new Item[size()];
+        copy=list;
+        if(size()+1>=(size()*2)){
+            list=new Item[size()*2];
+            list[copy.size()+1]=item;
+            for(int i=0;i<size()/2-1;i++){
+                list[i]=copy[i];
+            }
+        }
+        else{
+            list[copy.size()+1]=item;
+            for(int i=0;i<size()/2-1;i++){
+                list[i]=copy[i];
+            }
+        }
+    }
+
+    // remove and return the item from the front
+    public Item removeFirst(){
+        if(list.isEmpty()){
+            throw new java.util.NoSuchElementException("Stack is empty")
+        }
+        Item[] copy=new Item[size()];
+        copy=list;
+        if(size()-1<=(size()/4)){
+            list=new Item[size()/2];
+            for(int i=0;i<size()/4;i++){
+                list[i]=copy[i+1];
+            }
+        }
+        else{
+            for(int i=0;i<size()/4;i++){
+                list[i]=copy[i+1];
+            }
+        }
+    }
+
+    // remove and return the item from the back
+    public Item removeLast(){
+        if(list.isEmpty()){
+            throw new java.util.NoSuchElementException("Stack is empty")
+        }
+        Item[] copy=new Item[size()];
+        copy=list;
+        if(size()-1<=(size()/4)){
+            list=new Item[size()/2];
+            for(int i=0;i<size()/4-1;i++){
+                list[i]=copy[i];
+            }
+        }
+        else{
+            for(int i=0;i<size()/4-1;i++){
+                list[i]=copy[i];
+            }
+        }
+    }
+
+    // return an iterator over items in order from front to back
+    public Iterator<Item> iterator(){
+
+    }
+
+    // unit testing (required)
+    public static void main(String[] args)
+
+}
--- Stacks and Deques/Deque.java
+++ Stacks and Deques/Deque.java
@@ -1,19 +1,22 @@
+import java.util.Iterator;
+
 public class Deque<Item> implements Iterable<Item> {
-    public Item[] list;
     // construct an empty deque
     public Deque(){
-        list = new Item[1];
+        private Node first;
+        private Node last;
+        private int length;
 
     }
 
     // is the deque empty?
     public boolean isEmpty(){
-        return list[0] == null;
+        return first == null;
     }
 
     // return the number of items on the deque
     public int size(){
-        return list[].size();
+        return length;
     }
 
     // add the item to the front
@@ -21,21 +24,13 @@ public class Deque<Item> implements Iterable<Item> {
         if(item==null){
             throw new IllegalArgumentException("item is null");
         }
-        Item[] copy=new Item[size()];
-        copy=list;
-        if(size()+1>=(size()*2)){
-            list=new Item[size()*2];
-            list[0]=item;
-            for(int i=0;i<size()/2-1;i++){
-                list[i+1]=copy[i];
-            }
-        }
-        else{
-            list[0]=item;
-            for(int i=0;i<size()/2-1;i++){
-                list[i+1]=copy[i];
-            }
-        }
+        Item oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = null;
+        if (isEmpty()) last = first;
+        else oldfirst.next = first;
+        length+=1;
     }
 
     // add the item to the back
@@ -43,21 +38,14 @@ public class Deque<Item> implements Iterable<Item> {
         if(item==null){
             throw new IllegalArgumentException("item is null");
         }
-        Item[] copy=new Item[size()];
-        copy=list;
-        if(size()+1>=(size()*2)){
-            list=new Item[size()*2];
-            list[copy.size()+1]=item;
-            for(int i=0;i<size()/2-1;i++){
-                list[i]=copy[i];
-            }
-        }
-        else{
-            list[copy.size()+1]=item;
-            for(int i=0;i<size()/2-1;i++){
-                list[i]=copy[i];
-            }
-        }
+        Item oldlast = last;
+        last = new Node();
+        last.item = item;
+        last.next = null;
+        if (isEmpty()) first = last;
+        else oldlast.next = last;
+        length+=1;
+
     }
 
     // remove and return the item from the front
@@ -65,39 +53,22 @@ public class Deque<Item> implements Iterable<Item> {
         if(list.isEmpty()){
             throw new java.util.NoSuchElementException("Stack is empty")
         }
-        Item[] copy=new Item[size()];
-        copy=list;
-        if(size()-1<=(size()/4)){
-            list=new Item[size()/2];
-            for(int i=0;i<size()/4;i++){
-                list[i]=copy[i+1];
-            }
-        }
-        else{
-            for(int i=0;i<size()/4;i++){
-                list[i]=copy[i+1];
-            }
-        }
-    }
+        Item item = first.item;
+        first = first.next;
+        if (isEmpty()) last = null;
+        length-=1;
+        return item;
 
     // remove and return the item from the back
     public Item removeLast(){
         if(list.isEmpty()){
             throw new java.util.NoSuchElementException("Stack is empty")
         }
-        Item[] copy=new Item[size()];
-        copy=list;
-        if(size()-1<=(size()/4)){
-            list=new Item[size()/2];
-            for(int i=0;i<size()/4-1;i++){
-                list[i]=copy[i];
-            }
-        }
-        else{
-            for(int i=0;i<size()/4-1;i++){
-                list[i]=copy[i];
-            }
-        }
+        String item = last.item;
+        last = last.next;
+        if (isEmpty()) first = null;
+        length-=1;
+        return item;
     }
 
     // return an iterator over items in order from front to back
