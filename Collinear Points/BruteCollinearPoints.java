4c9ed390a901969692a730f0cb124c59a1916fd2
Eesha <73623905+eesha123456789@users.noreply.github.com>
2023-10-26 23:17:19 +0000

    .

--- Collinear Points/BruteCollinearPoints.java
+++ Collinear Points/BruteCollinearPoints.java
@@ -1,5 +1,12 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import java.util.Arrays;
+
 public class BruteCollinearPoints {
-   public BruteCollinearPoints(Point[] points)    // finds all line segments containing 4 points
-   public           int numberOfSegments()        // the number of line segments
-   public LineSegment[] segments()                // the line segments
+    private final Point[] girdPoints;
+    public BruteCollinearPoints(Point[] points){
+        gridPoints=points;
+    }    // finds all line segments containing 4 points
+    public           int numberOfSegments()        // the number of line segments
+    public LineSegment[] segments()                // the line segments
 }
--- Collinear Points/FastCollinearPoints.java
+++ Collinear Points/FastCollinearPoints.java
@@ -1,5 +1,12 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import java.util.Arrays;
+
 public class FastCollinearPoints {
-   public FastCollinearPoints(Point[] points)     // finds all line segments containing 4 or more points
-   public           int numberOfSegments()        // the number of line segments
-   public LineSegment[] segments()                // the line segments
+    private final Point[] gridPoints;
+    public FastCollinearPoints(Point[] points){
+        
+    }     // finds all line segments containing 4 or more points
+    public           int numberOfSegments()        // the number of line segments
+    public LineSegment[] segments()                // the line segments
 }
