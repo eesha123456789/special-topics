e9d8182f3d7c5c603c349ab6b8d22274631aa827
Eesha <73623905+eesha123456789@users.noreply.github.com>
2023-10-26 23:11:49 +0000

    .

---
+++ Collinear Points/Point.java
@@ -0,0 +1,58 @@
+import java.util.Comparator;
+import edu.princeton.cs.algs4.StdDraw;
+
+public class Point implements Comparable<Point> {
+    private final int x;
+    private final int y;
+    public Point(int x1, int y1){                       // constructs the point (x, y)
+        x=x1;
+        y=y1;
+    }
+    public void draw()   {
+        StdDraw.point(x, y);
+    }                          
+    public   void drawTo(Point that) {
+        StdDraw.line(x, y, that.x, that.y);
+    }                  
+    public String toString(){
+        return "(" + x + ", " + y + ")";
+    }                          
+
+    public int compareTo(Point that){
+        if(x==that.x && y==that.y){
+            return 0;
+        }
+        else if(y < that.y || (y==that.y && x<that.x)){
+            return -1;
+        }
+        else if(y>that.y){
+            return 1;
+        } 
+    }     // eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+   
+    public  double slopeTo(Point that){
+        if(x==that.x && y==that.y){
+            return Double.NEGATIVE_INFINITY;
+        }
+        else if(x==that.x){
+            return Double.POSITIVE_INFINITY;
+        }
+        else if(y==that.y){
+            return +0.0;
+        }
+        return (double) (that.y-y)/(that.x-x);
+    }    
+
+    public Comparator<Point> slopeOrder(){
+        
+    }              // compare two points by slopes they make with this point
+    
+
+}
+
+
+public class LineSegment {
+    public LineSegment(Point p, Point q)        // constructs the line segment between points p and q
+    public   void draw()                        // draws this line segment
+    public String toString()                    // string representation
+ }
