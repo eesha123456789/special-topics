49f92e5abf757e1526b390db86e4838bf4d631cb
Eesha <73623905+eesha123456789@users.noreply.github.com>
2023-12-03 04:25:11 +0000

    .

---  8puzzle/Board.java
+++  8puzzle/Board.java
@@ -1,16 +1,21 @@
+import java.util.LinkedList;
+
 public class Board {
 
     // create a board from an n-by-n array of tiles,
     // where tiles[row][col] = tile at (row, col)
-    private int[] board;
+    private int[][] board;
     private int n;
+    private int [] one_D;
 
     public Board(int[][] tiles){
         n=tiles.length;
-        board=new int[n*n];
+        board=new int[n][n];
+        one_D=new int[n*n];
         for(int i=0; i<n;i++){
             for(int j=0; j<n;j++){
-            board[i*n+j]=tiles[i][j];
+                board[i][j]=tiles[i][j];
+                one_D[i*n+j]=tiles[i][j];
             }
         }
     }
@@ -19,10 +24,10 @@ public class Board {
     public String toString(){
         String fin = "";
         for(int i=0;i<n*n;i++){
-            if(i%3==1){
-                fin+="\n";
+            for(int j=0; j<n;j++){
+                fin+=board[i][j];
             }
-            fin+=board[i];
+            fin+="\n";
         }
         return fin;
     }
@@ -36,7 +41,7 @@ public class Board {
     public int hamming(){
         int num =0;
         for (int i=0; i<n*n-1;i++){
-            if(board[i]+1!=board[i+1] && board[i]!= 0){
+            if(one_D[i]+1!=one_D[i+1] && one_D[i]!= 0){
                 num++;
             }
         }
@@ -47,8 +52,9 @@ public class Board {
     public int manhattan(){
         int num =0;
         for (int i=0; i<n*n-1;i++){
-            if(board[i]+1!=board[i+1] && board[i]!= 0){
-                num++;
+            if(one_D[i]+1!=one_D[i+1] && one_D[i]!= 0){
+                num+= (one_D[i]-1)/n-i/n;
+                num+=(one_D[i]-1)% n - i % n;
             }
         }
         return num;
@@ -56,8 +62,8 @@ public class Board {
 
     // is this board the goal board?
     public boolean isGoal(){
-        for (int i=0; i < n*n-1; i++){
-            if (board[i]!=i+1){
+        for (int i=0; i < n*n; i++){
+            if (one_D[i]!=i){
                 return false;
             }
         }
@@ -75,10 +81,53 @@ public class Board {
     }
 
     // all neighboring boards
-    public Iterable<Board> neighbors()
+    public Iterable<Board> neighbors(){
+        LinkedList<Board> fin = new LinkedList<Board>();
+        int[][] a = board;
+        if(one_D[0] != 0){
+            int temp = a[one_D[0]-1][one_D[1]];
+            a[one_D[0]-1][one_D[1]] = a[one_D[0]][one_D[1]];
+            a[one_D[0]][one_D[1]] = temp;
+            fin.add(new Board(a));
+        }
+        if(one_D[0] != 0){
+            int temp = a[one_D[0]+1][one_D[1]];
+            a[one_D[0]+1][one_D[1]] = a[one_D[0]][one_D[1]];
+            a[one_D[0]][one_D[1]] = temp;
+            fin.add(new Board(a));
+        }
+        if(one_D[0] != 0){
+            int temp = a[one_D[0]][one_D[1]-1];
+            a[one_D[0]][one_D[1]-1] = a[one_D[0]][one_D[1]];
+            a[one_D[0]][one_D[1]] = temp;
+            fin.add(new Board(a));
+        }
+        if(one_D[0] != 0){
+            int temp = a[one_D[0]][one_D[1]+1];
+            a[one_D[0]][one_D[1]+1] = a[one_D[0]][one_D[1]];
+            a[one_D[0]][one_D[1]] = temp;
+            fin.add(new Board(a));
+        }
+        return fin;
+
+    }
 
     // a board that is obtained by exchanging any pair of tiles
-    public Board twin()
+    public Board twin(){
+        int[][] a = board;
+        if(board[0][1] != 0 && board[0][0] != 0){
+            int temp = a[0][1];
+            a[0][1] = a[0][0];
+            a[0][0] = temp;
+            return new Board(a);
+        }
+        else{
+            int temp = a[1][1];
+            a[1][1] = a[1][0];
+            a[1][0] = temp;
+            return new Board(a);
+        }
+    }
 
     // unit testing (not graded)
     public static void main(String[] args){}
