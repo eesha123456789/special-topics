ae363eb01e91f91e9bf078d6db231595e4b7a742
Eesha <73623905+eesha123456789@users.noreply.github.com>
2024-01-26 18:02:55 +0000

    kd

--- Kd-Trees/KdTree.java
+++ Kd-Trees/KdTree.java
@@ -1,4 +1,6 @@
 import java.util.LinkedList;
+import java.util.List;
+
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 
@@ -7,51 +9,129 @@ public class KdTree {
     
    private class Node {
 		Point2D point;
-		Node left =null;
-		Node right=null;
+		Node left;
+		Node right;
 		boolean bool;
-
+		
+		Node(Point2D p) {
+			point = p;
+			left = null;
+			right = null;
+		}
 	}
+	
+	private Node start;
+	private LinkedList<Point2D> list;
+	private Point2D champion;
+	private Point2D comparison;
+	private int size;
+	
 
-   private Node begin;
-	private LinkedList<Point2D> list= new LinkedList<Point2D>();
-	private int length;
+	public KdTree() {
+		start = null;
+	} //Constructor, creates a SET of points
+	
+	
+	public boolean isEmpty() {
+		return size() == 0;
+	} //Returns true when there are no points in the set
+	
+	
+	public int size() {
+		if (start == null) {
+			return 0;
+		} else {
+			return getCount(start);
+		}
+	} //Returns the number of points in the set 
+	
+	private int getCount(Node node) {
+		return size;
+	}
+	
+	
+	public void insert(Point2D p) {
+		Node makeP = new Node(p);
+		Node position = start;
+		
+		if(start == null) {
+			makeP.bool = true;
+			start = makeP;
+			size++;
+			return;
+		}
+		
+		while(true) {
+			if(p.equals(position.point)) {
+				return;
+			}
+			
+			if (!position.bool) {
+				if (p.y() < position.point.y()) {
+					if (position.left == null) {
+						makeP.bool = true;
+						position.left = makeP;
+						size++;
+						return;
+					} else {
+						position = position.left;
+					}
+				} else {
+					if (position.right == null) {
+						makeP.bool = true;
+						position.right = makeP;
+						size++;
+						return;
+					} else {
+						position = position.right;
+					}
+				}
+			} else {
+				if (p.x() < position.point.x()) {
+					if (position.left == null) {
+						makeP.bool = false;
+						position.left = makeP;
+						size++;
+						return;
+					} else {
+						position = position.left;
+					}
+				} else {
+					if (position.right == null) {
+						makeP.bool = false;
+						position.right = makeP;
+						size++;
+						return;
+					} else {
+						position = position.right;
+					}
+				}
 
-   public KdTree()  {
-      begin=null;
-      length=0;
-   }                             // construct an empty set of points 
-   public boolean isEmpty(){
-      return begin==null;
-
-   }                      // is the set empty? 
-   public int size(){
-      return length;
-   }                         // number of points in the set 
-   public void insert(Point2D p){
-      
-   }              // add the point to the set (if it is not already in the set)
-   
+				
+			}
+		}
+	}
+	
    
    public boolean contains(Point2D p){
-      while (begin != null) {
-         if (begin.bool) {
-             if (p.x() > begin.point.x()) 
-               begin = begin.right;
-             else if (p.x() < begin.point.x()) 
-               begin = begin.left;
-             else if (p.y() != begin.point.y()) 
-               begin = begin.right;
+      while (start != null) {
+         if (start.bool) {
+             if (p.x() > start.point.x()) 
+               start = start.right;
+             else if (p.x() < start.point.x()) 
+               start = start.left;
+             else if (p.y() != start.point.y()) 
+               start = start.right;
              else 
                return true;
          }
-         else if (!begin.bool) {
-             if (p.y() > begin.point.y()) 
-               begin = begin.right;
-             else if (p.y() < begin.point.y()) 
-               begin = begin.left;
-             else if (p.x() != begin.point.x()) 
-               begin = begin.right;
+         else if (!start.bool) {
+             if (p.y() > start.point.y()) 
+               start = start.right;
+             else if (p.y() < start.point.y()) 
+               start = start.left;
+             else if (p.x() != start.point.x()) 
+               start = start.right;
              else 
                return true;
          }
@@ -61,19 +141,57 @@ public class KdTree {
 
 
    public void draw(){
-      if (begin != null) {
-         draw(begin);
+      if (start != null) {
+         draw(start);
       }
    }                      
    
 
-   public Iterable<Point2D> range(RectHV rect){
-		rangeFunc(begin, rect);
-		return list;
-   }             // all points that are inside the rectangle (or on the boundary) 
-   public Point2D nearest(Point2D p){
-      return null;
-   }             // a nearest neighbor in the set to point p; null if the set is empty 
+   public Iterable<Point2D> range(RectHV rect) {
+	list = new LinkedList<Point2D>();
+	rangeFunc(start, rect);
+	return list;
+	}
+
+	private void rangeFunc(Node node, RectHV rect) {
+		if (node == null) return;
+		
+		if(node.bool) {
+			if(node.point.x() > rect.xmax()) 
+				rangeFunc(node.left, rect);
+				
+			else if (node.point.x() < rect.xmin()) 
+				rangeFunc(node.right, rect);
+				
+			else {
+				rangeFunc(node.left, rect);
+				rangeFunc(node.right, rect);
+				
+				if(rect.contains(node.point)) {
+					list.add(node.point);
+				}
+			}
+			
+		} else {
+			if(node.point.y() > rect.ymax()) 
+				rangeFunc(node.left, rect);
+				
+			else if (node.point.y() < rect.ymin()) 
+				rangeFunc(node.right, rect);
+				
+			else {
+				rangeFunc(node.left, rect);
+				rangeFunc(node.right, rect);
+				
+				if(rect.contains(node.point)) {
+					list.add(node.point);
+				}
+			}	
+		}
+	}       // all points that are inside the rectangle (or on the boundary) 
+	public Point2D nearest(Point2D p){
+		return null;
+	}             // a nearest neighbor in the set to point p; null if the set is empty 
 
 
    private void draw(Node temp) {
@@ -88,42 +206,8 @@ public class KdTree {
 		}
 	}
 
-   private void rangeFunc(Node tempNode, RectHV rect) {
-		if (tempNode == null) return;
-		
-		if(tempNode.bool) {
 
-			if(tempNode.point.x() > rect.xmax()) 
-            rangeFunc(tempNode.left, rect);
-				
-			else if (tempNode.point.x() < rect.xmin()) 
-            rangeFunc(tempNode.right, rect);
-				
-			else {
-				rangeFunc(tempNode.left, rect);
-				rangeFunc(tempNode.right, rect);
-				if(rect.contains(tempNode.point)) {
-					list.add(tempNode.point);
-				}
-			}
-		} 
-      else {
 
-			if(tempNode.point.y() > rect.ymax())
-            rangeFunc(tempNode.left, rect);
-				
-			else if (tempNode.point.y() < rect.ymin())
-            rangeFunc(tempNode.right, rect);
-				
-			else {
-				rangeFunc(tempNode.left, rect);
-				rangeFunc(tempNode.right, rect);
-				if(rect.contains(tempNode.point)) {
-					list.add(tempNode.point);
-				}
-			}	
-		}
-	}
 
    public static void main(String[] args) {}                 // unit testing of the methods (optional) 
 }
--- Kd-Trees/PointSET.java
+++ Kd-Trees/PointSET.java
@@ -4,48 +4,77 @@ import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.SET;
 
 public class PointSET {
-   private SET<Point2D> set;
-   public PointSET(){                               // construct an empty set of points {
-      set = new SET<Point2D>();
-   }
-   public boolean isEmpty(){                      // is the set empty? 
-      return set.isEmpty();
-   }
-   public int size(){                       // number of points in the set 
-      return set.size();
-   }
-   public void insert(Point2D p){              // add the point to the set (if it is not already in the set)
-         set.add(p);
-   }
-   public boolean contains(Point2D p){            // does the set contain point p? 
-      return set.contains(p);
-   }
-   public void draw(){
-      for (Point2D i : set)
+    private SET<Point2D> points;
+
+    public PointSET() {  // construct an empty set of points 
+        points = new SET<Point2D>();
+    }     
+    
+    
+    public boolean isEmpty() { // is the set empty?   
+        return points.isEmpty();
+    }            
+    
+    public int size() {     // number of points in the set 
+        return points.size();
+    }   
+    
+    
+    public void insert(Point2D p) {     // add the point to the set (if it is not already in the set)
+        if (p==null) 
+            throw new IllegalArgumentException();
+
+        if (!points.contains(p)) {
+            points.add(p);
+        }   
+    }       
+    
+     
+    public boolean contains(Point2D p) {       // does the set contain point p?
+        if (p == null)
+            throw new IllegalArgumentException();
+        
+        return points.contains(p);
+    }   
+    
+    public void draw() {       // draw all points to standard draw 
+        for (Point2D i : points)
             i.draw();
-   }                         // draw all points to standard draw 
-   
-   public Iterable<Point2D> range(RectHV rect){             // all points that are inside the rectangle (or on the boundary) 
-      Stack<Point2D> fin = new Stack<Point2D>();
-      for(Point2D i : set){
-         if(rect.contains(i)){
-            fin.push(i);
-         }
-      }
+    } 
+    
+     // all points that are inside the rectangle (or on the boundary) 
+    public Iterable<Point2D> range(RectHV rect) { 
+        if (rect==null) 
+            throw new IllegalArgumentException();
+        
+        Stack<Point2D> list = new Stack<Point2D>();
+        for (Point2D p : points) {
+            if (p.x() >= rect.xmin() && p.x() <= rect.xmax() && p.y() >= rect.ymin() && p.y() <= rect.ymax()) {
+                list.push(p);
+            }
+        }
+        return list;
+    }  
+    
+     
+    public Point2D nearest(Point2D p) {     // a nearest neighbor in the set to point p; null if the set is empty
+        if (p==null) 
+            throw new IllegalArgumentException();
+        if (points.isEmpty())
+            return null;
+        
 
-      return fin;
-   }
-   public Point2D nearest(Point2D p) {
-      Point2D fin = null;
-      for(Point2D i: set){
-         if(fin==null)
-            fin=i;
-         else if(p.distanceTo(i) < p.distanceTo(fin)){
-            fin=i;
-         }
-      }
-      return fin;
-   }            // a nearest neighbor in the set to point p; null if the set is empty 
-
-   //public static void main(String[] args)                  // unit testing of the methods (optional) 
-}
+        double dist = 0xffff;
+        Point2D close = null;
+        for (Point2D temp : points) {
+            if (temp.distanceSquaredTo(p) < dist) {
+                dist = temp.distanceSquaredTo(p);
+                close = temp;
+            }
+        }
+        return close;
+    }             
+ 
+    // unit testing of the methods (optional) 
+    public static void main(String[] args)  { }                
+ }
