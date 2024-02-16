1c900d5be8245fab778b380d0e15ac58119af190
Eesha <73623905+eesha123456789@users.noreply.github.com>
2024-02-16 04:12:12 +0000

    .

---
+++ Word Net/SAP.java
@@ -0,0 +1,45 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+import edu.princeton.cs.algs4.BreadthFirstDirectedPaths;
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
+
+
+public class SAP {
+    private final Digraph main;
+    private final HashMap<HashSet<Integer>, int[]> softwareCache;
+    public SAP(Digraph G){
+        main = new Digraph(G);
+        softwareCache = new HashMap<HashSet<Integer>, int[]>();
+    }
+ 
+    // length of shortest ancestral path between v and w; -1 if no such path
+    public int length(int v, int w){
+        HashSet<Integer> pointer = new HashSet<>();
+        pointer.add(v);
+        pointer.add(w);
+        int[] fin = softwareCache.get(pointer);
+        return fin[0];
+    }
+ 
+    // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
+    public int ancestor(int v, int w){
+        return -1;
+    }
+ 
+    // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
+    public int length(Iterable<Integer> v, Iterable<Integer> w){
+
+    }
+ 
+    // a common ancestor that participates in shortest ancestral path; -1 if no such path
+    public int ancestor(Iterable<Integer> v, Iterable<Integer> w){
+        
+    }
+ 
+    // do unit testing of this class
+    public static void main(String[] args){}
+ }
+ 
--- Word Net/WordNet.java
+++ Word Net/WordNet.java
@@ -1,21 +1,37 @@
+import java.util.HashMap;
+
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.DirectedCycle;
+
 public class WordNet {
+    private final Digraph wordNet;
+    private final HashMap<String, Bag<Integer>> synsetIds;
+    private final HashMap<Integer, String> synsetStore;
+    private final SAP sap;
 
     // constructor takes the name of the two input files
-    public WordNet(String synsets, String hypernyms)
+    public WordNet(String synsets, String hypernyms){
+        
+    }
  
     // returns all WordNet nouns
-    public Iterable<String> nouns()
+    public Iterable<String> nouns(){
+        return synsetIds.keySet();
+    }
  
     // is the word a WordNet noun?
-    public boolean isNoun(String word)
+    public boolean isNoun(String word){}
  
     // distance between nounA and nounB (defined below)
-    public int distance(String nounA, String nounB)
+    public int distance(String nounA, String nounB){}
  
     // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
     // in a shortest ancestral path (defined below)
-    public String sap(String nounA, String nounB)
+    public String sap(String nounA, String nounB){
+
+    }
  
     // do unit testing of this class
-    public static void main(String[] args)
+    public static void main(String[] args){}
  }
